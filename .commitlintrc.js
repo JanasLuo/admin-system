b32e74ce29a3664e3c13c4e36215b65644465c8f
janasluo <janas_luo@126.com>
2021-08-19 12:07:11 +0800

    feat: commit效验

---
+++ .commitlintrc.js
@@ -0,0 +1,25 @@
+/*
+ * @Descripttion:
+ * @version:
+ * @Author: luolei
+ * @Date: 2021-05-06 18:07:05
+ * @LastEditors: luolei
+ * @LastEditTime: 2021-05-06 19:29:57
+ */
+module.exports = {
+  extends: [
+    '@commitlint/config-conventional'
+  ],
+  rules: {
+    'type-enum': [2, 'always', [
+      'feat', 'update', 'fix', 'refactor', 'perf', 'style', 'docs', 'chore', 'improve', 'revert', 'test', 'build', 'merge'
+    ]],
+    'type-case': [0],
+    'type-empty': [0],
+    'scope-empty': [0],
+    'scope-case': [0],
+    'subject-full-stop': [0, 'never'],
+    'subject-case': [0, 'never'],
+    'header-max-length': [0, 'always', 72]
+  }
+};
