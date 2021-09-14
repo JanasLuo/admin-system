a0b6878b585f1104f26e9dc2ce14b0c0af293b3c
janasluo <janas_luo@126.com>
2021-09-14 22:46:15 +0800

    update: lint

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,7 +1,7 @@
 /*
  * @Author: janasluo
  * @Date: 2021-09-13 11:08:20
- * @LastEditTime: 2021-09-14 22:03:39
+ * @LastEditTime: 2021-09-14 22:46:12
  * @LastEditors: janasluo
  * @Description:
  * @FilePath: /digital_police/Users/janas/work/project/frontend/admin-system/src/pages/App.tsx
