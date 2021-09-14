c22bf56d5d09a92914a09742e992dfbf2abab9bd
janasluo <janas_luo@126.com>
2021-09-14 21:59:30 +0800

    improve: prettier

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -19,7 +19,7 @@ import zh_CN from 'antd/lib/locale-provider/zh_CN'
 class App extends React.Component<{}, {}> {
   @observable public loaderStore: LoaderStore
 
-  constructor(props: any) {
+  constructor(props: any){
     super(props)
     this.loaderStore=props.loaderStore;
     // if (!props.userStore.getAccount()) {
@@ -33,7 +33,7 @@ class App extends React.Component<{}, {}> {
     return (
       <ConfigProvider locale={zh_CN}>
         <div className="app">
-          {this.props.children}
+              {this.props.children}
           <div
             className={`loading-box ${
               this.loaderStore.getLoading ? 'show' : ''
