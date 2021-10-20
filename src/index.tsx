d96d804c09183f59375c79be29fedc8897c5e520
janasluo <janas_luo@126.com>
2021-10-20 14:53:40 +0800

    fix: remove App

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
+/*
+ * @Author: janasluo
+ * @Date: 2021-10-19 17:55:42
+ * @LastEditTime: 2021-10-20 14:52:31
+ * @LastEditors: janasluo
+ * @Description:
+ * @FilePath: /digital_police/Users/janas/work/project/frontend/admin-system/src/index.tsx
+ */
+// import React from 'react'
+// import ReactDOM from 'react-dom'
+// import App from './App'
 
 export { default as Button } from './components/Button'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+// ReactDOM.render(<App />, document.getElementById('root'))
