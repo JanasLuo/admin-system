a028234a6790329a10a2de99de5b8947f6d2a36e
janasluo <janas_luo@126.com>
2021-11-15 21:38:10 +0800

    improve: webpack优化

--- webpack.config.js
+++ webpack.config.js
@@ -4,17 +4,18 @@
  * @Author: liuhaoran
  * @Date: 2021-01-15 11:35:57
  * @LastEditors: janasluo
- * @LastEditTime: 2021-09-19 20:46:52
+ * @LastEditTime: 2021-11-15 21:36:59
  */
 const proxyObject = require('./config/proxy.conf')
 const getBuildInfo = require('./version.js')
 // const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
 //   .BundleAnalyzerPlugin
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 const path = require('path')
 const fs = require('fs')
-
+// 分析打包时间
+// const SpeedMeasurePlugin = require("speed-measure-webpack-plugin");
+// const smp = new SpeedMeasurePlugin();
 // 获取文件路径
 function getFileRealPath(s) {
   try {
@@ -25,23 +26,20 @@ function getFileRealPath(s) {
 }
 module.exports = {
   webpack: (config, env) => {
+    debugger
     config.module.rules = config.module.rules.map(rule => {
       if (rule.oneOf instanceof Array) {
         return {
           ...rule,
           oneOf: [
             {
-              test: /\.(jsx|js|ts|tsx)$/,
-              include: [path.resolve(__dirname, '../src')],
-              exclude: [/node_modules/],
-              use: ['eslint-loader'],
-              enforce: 'pre'
-            },
-            {
               test: /\.styl$/,
               use: [
                 {
                   loader: 'style-loader'
+                },
+                {
+                  loader: 'thread-loader'
                 },
                 {
                   loader: 'css-loader',
@@ -60,30 +58,19 @@ module.exports = {
       }
       return rule
     })
-
+    // thread-loader多实例构建;cache-loader减少二次打包时间
+    config.module.rules.push({
+      test: /\.(jsx|js|ts|tsx)$/,
+      include: [path.resolve(__dirname, '../src')],
+      exclude: [/node_modules/],
+      use: ['cache-loader', 'thread-loader']
+    })
     if (env === 'production') {
       delete config.devtool
       // config.plugins.push(new BundleAnalyzerPlugin()) // 打包分析
-      const plugins = [
-        // 去掉console.log debugger
-        new UglifyJsPlugin({
-          uglifyOptions: {
-            // 删除注释
-            output: {
-              comments: false
-            },
-            // 删除console debugger 删除警告
-            compress: {
-              drop_console: true,
-              drop_debugger: true,
-              pure_funcs: ['console.log'] // 移除console
-            }
-          }
-        })
-      ]
       // 通过获取.git文件路径 判断是否在git目录下
       if (getFileRealPath('.git')) {
-        plugins.push(
+        config.plugins.push(
           new HtmlWebpackPlugin({
             filename: './version.html', // 打包后生成的文件路径
             template: './version/index.html', // 需要处理的对象
@@ -97,10 +84,28 @@ module.exports = {
           })
         )
       }
-      config.plugins = [...config.plugins, ...plugins]
     }
-    // config.externals = ['canvas']
+    if (env === 'development') {
+      // 开发环境代码检查
+      config.module.rules.push({
+        test: /\.(jsx|js|ts|tsx)$/,
+        include: [path.resolve(__dirname, '../src')],
+        exclude: [/node_modules/],
+        use: ['eslint-loader'],
+        enforce: 'pre'
+      })
+      config.devtool = 'cheap-module-eval-source-map'
+    }
+
+    const terserOptions = config.optimization.minimizer[0].options.terserOptions
+    terserOptions.compress = {
+      ...terserOptions.compress,
+      drop_console: true,
+      drop_debugger: true,
+      pure_funcs: ['console.log'] // 移除console
+    }
     return config
+    // return smp.wrap(config)
   },
   devServer: function (configFunction, env) {
     if (env === 'development') {
